b0377f9fcc7b8433f37d505d9473fd01ed5fd778
Marvin Diaz <marvindaviddiaz@gmail.com>
2019-05-12 21:09:44 -0600

    Prueba Hibernate

---
+++ arquitectura-serverless/buscar-servicio/src/test/java/com/github/marvindaviddiaz/BuscarServicioTest.java
@@ -0,0 +1,72 @@
+package com.github.marvindaviddiaz;
+
+import com.amazonaws.services.lambda.runtime.ClientContext;
+import com.amazonaws.services.lambda.runtime.CognitoIdentity;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import org.junit.Test;
+
+public class BuscarServicioTest {
+
+    @Test
+    public void buscar_servicio_ok() {
+        BuscarServicio servicio = new BuscarServicio();
+        servicio.handleRequest("test", new Context() {
+            @Override
+            public String getAwsRequestId() {
+                return null;
+            }
+
+            @Override
+            public String getLogGroupName() {
+                return null;
+            }
+
+            @Override
+            public String getLogStreamName() {
+                return null;
+            }
+
+            @Override
+            public String getFunctionName() {
+                return null;
+            }
+
+            @Override
+            public String getFunctionVersion() {
+                return null;
+            }
+
+            @Override
+            public String getInvokedFunctionArn() {
+                return null;
+            }
+
+            @Override
+            public CognitoIdentity getIdentity() {
+                return null;
+            }
+
+            @Override
+            public ClientContext getClientContext() {
+                return null;
+            }
+
+            @Override
+            public int getRemainingTimeInMillis() {
+                return 0;
+            }
+
+            @Override
+            public int getMemoryLimitInMB() {
+                return 0;
+            }
+
+            @Override
+            public LambdaLogger getLogger() {
+                return null;
+            }
+        });
+    }
+
+}
