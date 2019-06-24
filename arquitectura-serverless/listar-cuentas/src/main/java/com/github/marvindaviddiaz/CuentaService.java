9e89d88f1cef5ebeb67a68612b41a46c5c6fe677
Marvin Diaz <marvindaviddiaz@gmail.com>
2019-06-23 20:20:26 -0600

    Seguridad

--- arquitectura-serverless/listar-cuentas/src/main/java/com/github/marvindaviddiaz/CuentaService.java
+++ arquitectura-serverless/listar-cuentas/src/main/java/com/github/marvindaviddiaz/CuentaService.java
@@ -7,6 +7,7 @@ import com.github.marvindaviddiaz.dao.CuentaDAO;
 import com.github.marvindaviddiaz.dto.CuentaDTO;
 
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,17 +18,9 @@ public class CuentaService implements RequestHandler<APIGatewayProxyRequestEvent
 
     @Override
     public List<CuentaDTO> handleRequest(APIGatewayProxyRequestEvent event, Context context) {
-        logger.log(Level.INFO, "Contexto: {0}", context);
-        logger.log(Level.INFO, "Identity: {0}", context.getIdentity());
-        logger.log(Level.INFO, "Pool: {0}", context.getIdentity().getIdentityPoolId());
-        logger.log(Level.INFO, "CognitoAuthenticationProvider: {0}", event.getRequestContext().getIdentity().getCognitoAuthenticationProvider());
-        logger.log(Level.INFO, "CognitoAuthenticationProviderType: {0}", event.getRequestContext().getIdentity().getCognitoAuthenticationType());
-        logger.log(Level.INFO, "CognitoIdentityId: {0}", event.getRequestContext().getIdentity().getCognitoIdentityId());
-        logger.log(Level.INFO, "CognitoIdentityPoolId: {0}", event.getRequestContext().getIdentity().getCognitoIdentityPoolId());
-        logger.log(Level.INFO, "User: {0}", event.getRequestContext().getIdentity().getUser());
         logger.log(Level.INFO, "Authorizer: {0}", event.getRequestContext().getAuthorizer());
-        Integer usuario = Integer.valueOf(event.getRequestContext().getIdentity().getCognitoIdentityId());
-        return cuentaDAO.listaCuentas(usuario);
+        String usuario = (String) ((Map)event.getRequestContext().getAuthorizer().get("claims")).get("cognito:username");
+        return cuentaDAO.listaCuentas(Integer.valueOf(usuario));
     }
 
 }
