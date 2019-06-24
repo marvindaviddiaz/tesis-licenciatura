5cbce1671918074e65f28f70ef8e604d46bb8a3c
Marvin Diaz <marvindaviddiaz@gmail.com>
2019-06-23 20:28:53 -0600

    Seguridad

--- arquitectura-serverless/listar-cuentas/src/main/java/com/github/marvindaviddiaz/CuentaService.java
+++ arquitectura-serverless/listar-cuentas/src/main/java/com/github/marvindaviddiaz/CuentaService.java
@@ -20,7 +20,10 @@ public class CuentaService implements RequestHandler<APIGatewayProxyRequestEvent
     public List<CuentaDTO> handleRequest(APIGatewayProxyRequestEvent event, Context context) {
         logger.log(Level.INFO, "Authorizer: {0}", event.getRequestContext().getAuthorizer());
         String usuario = (String) ((Map)event.getRequestContext().getAuthorizer().get("claims")).get("cognito:username");
-        return cuentaDAO.listaCuentas(Integer.valueOf(usuario));
+        logger.log(Level.INFO, "User: {0}", usuario);
+        List<CuentaDTO> cuentaDTOS = cuentaDAO.listaCuentas(Integer.valueOf(usuario));
+        logger.log(Level.INFO, "Result: {0}", cuentaDTOS);
+        return cuentaDTOS;
     }
 
 }
