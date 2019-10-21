0d6f8361769e21a7b990c82a53ecf967b5855fe3
Marvin Diaz <marvindaviddiaz@gmail.com>
2019-10-21 11:09:35 -0600

    Búsqueda de servicio

--- arquitectura-serverless/vista/src/app/buscar-servicio/buscar-servicio.service.ts
+++ arquitectura-serverless/vista/src/app/buscar-servicio/buscar-servicio.service.ts
@@ -14,6 +14,6 @@ export class BuscarServicioService {
   constructor(private http: HttpClient) { }
 
   query(query: string): Observable<any> {
-    return this.http.post(this.apiUrl, query);
+    return this.http.get(this.apiUrl, {params : {"busqueda" : query}});
   }
 }
