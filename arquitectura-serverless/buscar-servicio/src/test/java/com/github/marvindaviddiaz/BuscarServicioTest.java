6707b6dc16601e3db6d21a0ca9cdf833776dccab
Marvin Diaz <marvindaviddiaz@gmail.com>
2020-03-22 09:05:23 -0600

    Java 11

--- arquitectura-serverless/buscar-servicio/src/test/java/com/github/marvindaviddiaz/BuscarServicioTest.java
+++ arquitectura-serverless/buscar-servicio/src/test/java/com/github/marvindaviddiaz/BuscarServicioTest.java
@@ -1,12 +1,8 @@
 package com.github.marvindaviddiaz;
 
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.github.marvindaviddiaz.dto.ServicioDTO;
 import org.junit.Assert;
 import org.junit.Test;
-
-import javax.xml.bind.JAXB;
-import java.util.List;
 
 public class BuscarServicioTest {
 
