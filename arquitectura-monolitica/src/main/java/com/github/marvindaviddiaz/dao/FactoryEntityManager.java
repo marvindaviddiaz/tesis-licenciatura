38016178c1a97e81d0bbe013391522247cb5dfba
Marvin Diaz <marvindaviddiaz@gmail.com>
2021-05-31 20:31:33 -0600

    environment variables

--- arquitectura-monolitica/src/main/java/com/github/marvindaviddiaz/dao/FactoryEntityManager.java
+++ arquitectura-monolitica/src/main/java/com/github/marvindaviddiaz/dao/FactoryEntityManager.java
@@ -3,6 +3,8 @@ package com.github.marvindaviddiaz.dao;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FactoryEntityManager {
 
@@ -23,7 +25,20 @@ public class FactoryEntityManager {
 
     public EntityManagerFactory getEntityManagerFactory(){
         if(entityManagerFactory == null || ! entityManagerFactory.isOpen()){
-            return entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_NAME);
+            Map<String, String> env = System.getenv();
+            Map<String, Object> configOverrides = new HashMap<>();
+            if (env.containsKey("RDS_ENDPOINT")) {
+                configOverrides.put("javax.persistence.jdbc.url", String.format("jdbc:mysql://%s/%s",
+                        env.get("RDS_ENDPOINT"), env.get("RDS_DB_NAME")));
+            }
+            if (env.containsKey("RDS_USERNAME")) {
+                configOverrides.put("javax.persistence.jdbc.user", env.get("RDS_USERNAME"));
+            }
+            if (env.containsKey("RDS_PASSWORD")) {
+                configOverrides.put("javax.persistence.jdbc.password", env.get("RDS_PASSWORD"));
+            }
+            // You can put more code in here to populate configOverrides...
+            return entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_NAME, configOverrides);
         }
         return entityManagerFactory;
     }
